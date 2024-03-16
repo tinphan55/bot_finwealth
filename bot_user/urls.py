1ee3a6da9cdda7e236d4722c40ca0874b79a15a7
tinphan <tinph.1992@gmail.com>
2024-03-16 23:16:57 +0700

    add api detail username

--- bot_user/urls.py
+++ bot_user/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from .views import UserInfoListAPIView
+from .views import *
 
 urlpatterns = [
     path('api/userinfo/', UserInfoListAPIView.as_view(), name='userinfo-list'),
-    # ...
+    path('api/userinfo/<str:username>/', UserInfoDetailAPIView.as_view(), name='userinfo-detail'),
 ]
--- bot_user/views.py
+++ bot_user/views.py
@@ -8,3 +8,8 @@ from .serializers import UserInfoSerializer
 class UserInfoListAPIView(generics.ListAPIView):
     queryset = UserInfo.objects.all()
     serializer_class = UserInfoSerializer
+
+class UserInfoDetailAPIView(generics.RetrieveAPIView):
+    queryset = UserInfo.objects.all()
+    serializer_class = UserInfoSerializer
+    lookup_field = 'username'
